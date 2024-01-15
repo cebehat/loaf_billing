7f66fe31eb9696b5db8f99ca2e1a47eccfa81aad
cebehat <kontakt@cebt.dk>
2024-01-15 19:10:54 +0100

    dont save sig

--- shared/config.lua
+++ shared/config.lua
@@ -1,5 +1,5 @@
 Config = {
-    Framework = "auto", -- esx or qb
+    Framework = "qb", -- esx or qb
 
     -- esx options:
     MenuAlign = "bottom-right", -- the esx_menu_default menu location
@@ -15,7 +15,7 @@ Config = {
     MinAmount = 0, -- minimum bill amount
     MaxAmount = 900000000, -- max bill amount
     Signature = {
-        SaveSignature = true,
+        SaveSignature = false,
         SaveAs = "imgur", --[[
             * base64: image will be saved as base64 in your database (takes a lot of storage)
             * imgur: image will be uploaded to imgur (put your API key in server/config.lua)
